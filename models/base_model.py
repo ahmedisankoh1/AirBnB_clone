55bdb86026631064e8edb7e764cc08e9a88f514b
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2024-02-11 15:32:10 -0800

    hi

--- models/base_model.py
+++ models/base_model.py
@@ -34,9 +34,9 @@ class BaseModel:
             for key, value in kwargs.items():
                 setattr(self, key, value)
         else:
-            id = str(uuid.uuid4())
-            created_at = datetime.today()
-            updated_at = datetime.today()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
 
     def save(self):
         """Update updated_at with the current datetime."""
