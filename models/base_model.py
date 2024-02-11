ac267549fc0be6e5d0126908c8bf77769cfc4d44
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2024-02-11 13:34:14 -0800

    this

--- models/base_model.py
+++ models/base_model.py
@@ -8,29 +8,29 @@ from datetime import datetime
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
 
-    id = str(uuid4())
+    id = str(uuid.uuid4())
     created_at = datetime.today()
     updated_at = datetime.today()
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """Updates updated_at with the current datetime."""
 
         self.updated_at = datetime.today()
 
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
+        """Returns the dictionary of the BaseModel instance.
 
-        Includes the key/value pair __class__ representing
+        Includes the key-value pair __class__ representing
         the class name of the object.
         """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        clsdict = self.__dict__.copy()
+        clsdict["created_at"] = self.created_at.isoformat()
+        clsdict["updated_at"] = self.updated_at.isoformat()
+        clsdict["__class__"] = self.__class__.__name__
+        return clsdict
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
+        """Returns the printed representation of the BaseModel instance in a neat way."""
 
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        clsname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsname, self.id, self.__dict__)
